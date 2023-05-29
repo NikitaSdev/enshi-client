2aa3d8394842dcc07d48c690cff20db2d8759ac0
NikitaSdev <nikitashadowrazev@gmail.com>
2023-05-29 22:46:32 +0300

    loader done

---
+++ src/components/ui/SkeletonGallery/SkeletonGallery.tsx
@@ -0,0 +1,92 @@
+import Skeleton from "react-loading-skeleton"
+
+import styles from "./SkeletonGallery.module.scss"
+
+const SkeletonGallery = () => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.skeleton}>
+				<div className={styles.bigSkeleton}>
+					<Skeleton count={1} width={200} height={270} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={40}
+						baseColor={"#E3E3E3"}
+						className={styles.rounded}
+					/>
+				</div>
+				<div>
+					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+				</div>
+			</div>
+			<div className={styles.skeleton}>
+				<div className={styles.bigSkeleton}>
+					<Skeleton count={1} width={200} height={270} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={40}
+						baseColor={"#E3E3E3"}
+						className={styles.rounded}
+					/>
+				</div>
+				<div>
+					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+				</div>
+			</div>
+			<div className={styles.skeleton}>
+				<div className={styles.bigSkeleton}>
+					<Skeleton count={1} width={200} height={270} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={40}
+						baseColor={"#E3E3E3"}
+						className={styles.rounded}
+					/>
+				</div>
+				<div>
+					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+				</div>
+			</div>
+			<div className={styles.skeleton}>
+				<div className={styles.bigSkeleton}>
+					<Skeleton count={1} width={200} height={270} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={40}
+						baseColor={"#E3E3E3"}
+						className={styles.rounded}
+					/>
+				</div>
+				<div>
+					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+				</div>
+			</div>
+			<div className={styles.skeleton}>
+				<div className={styles.bigSkeleton}>
+					<Skeleton count={1} width={200} height={270} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={40}
+						baseColor={"#E3E3E3"}
+						className={styles.rounded}
+					/>
+				</div>
+				<div>
+					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default SkeletonGallery
