1d6460ce0afa464113870fbef9ad67f28367d3ff
NikitaSdev <nikitashadowrazev@gmail.com>
2023-06-01 02:52:37 +0300

    final pravki

--- src/components/ui/homeGallery/HomeGallery.tsx
+++ src/components/ui/homeGallery/HomeGallery.tsx
@@ -46,116 +46,118 @@ const HomeGallery: FC<{
 				[styles.announcedWrapper]: announced
 			})}
 		>
-			<div className={styles.headContainer}>
-				<h1 className={styles.heading}>
-					{heading}
+			<div>
+				<div className={styles.headContainer}>
+					<h1 className={styles.heading}>
+						{heading}
 
-					{icon && (
-						<span>
-							<Image src={icon} />
-						</span>
-					)}
-				</h1>
-			</div>
-			<div className={styles.swiperContainer}>
-				<button
-					onClick={prevSlide}
-					className={styles.button}
-					disabled={swiperNavigationAllowed.prevButtonDisabled}
-				>
-					<MaterialIcon name={"MdChevronLeft"} />
-				</button>
-				{
-					<Swiper
-						breakpoints={{
-							0: {
-								slidesPerView: 1
-							},
-							360: {
-								slidesPerView: 2
-							},
-							700: {
-								slidesPerView: 3
-							},
-							1050: {
-								slidesPerView: 4
-							},
-							1250: {
-								slidesPerView: 5
-							}
-						}}
-						modules={[Navigation]}
-						onBeforeInit={(swiper: SwiperCore | undefined) => {
-							swiperRef.current = swiper
-						}}
-						spaceBetween={20}
-						className={styles.swiper}
+						{icon && (
+							<span>
+								<Image src={icon} />
+							</span>
+						)}
+					</h1>
+				</div>
+				<div className={styles.swiperContainer}>
+					<button
+						onClick={prevSlide}
+						className={styles.button}
+						disabled={swiperNavigationAllowed.prevButtonDisabled}
 					>
-						{items ? (
-							items.list.map((item: IMovie) => (
-								<SwiperSlide key={uuidv4()} className={styles.swiperItem}>
-									<div className={styles.item}>
-										{!announced && (
-											<div className={styles.favourite}>
-												<MaterialIcon name={"MdBookmark"} />
-											</div>
-										)}
+						<MaterialIcon name={"MdChevronLeft"} />
+					</button>
+					{
+						<Swiper
+							breakpoints={{
+								0: {
+									slidesPerView: 1
+								},
+								360: {
+									slidesPerView: 2
+								},
+								700: {
+									slidesPerView: 3
+								},
+								1050: {
+									slidesPerView: 4
+								},
+								1250: {
+									slidesPerView: 5
+								}
+							}}
+							modules={[Navigation]}
+							onBeforeInit={(swiper: SwiperCore | undefined) => {
+								swiperRef.current = swiper
+							}}
+							spaceBetween={20}
+							className={styles.swiper}
+						>
+							{items ? (
+								items.list.map((item: IMovie) => (
+									<SwiperSlide key={uuidv4()} className={styles.swiperItem}>
+										<div className={styles.item}>
+											{!announced && (
+												<div className={styles.favourite}>
+													<MaterialIcon name={"MdBookmark"} />
+												</div>
+											)}
 
-										<img
-											alt={item.title}
-											src={
-												announced
-													? item.posters.original.url
-													: item.material_data.poster_url
-											}
-											draggable={false}
-										/>
-										{announced && (
-											<div className={styles.announce}>Анонсировано</div>
-										)}
-										{announced ? (
-											<div className={styles.description}>
-												<h3>{title(item.names.ru)}</h3>
-												<div className={styles.release}>{item.release}</div>
-											</div>
-										) : (
-											<div className={styles.description}>
-												<h3>{title(item.title)}</h3>
-												<div>
-													<div className={styles.year}>
-														<p>{item.year}</p>
-													</div>
+											<img
+												alt={item.title}
+												src={
+													announced
+														? item.posters.original.url
+														: item.material_data.poster_url
+												}
+												draggable={false}
+											/>
+											{announced && (
+												<div className={styles.announce}>Анонсировано</div>
+											)}
+											{announced ? (
+												<div className={styles.description}>
+													<h3>{title(item.names.ru)}</h3>
+													<div className={styles.release}>{item.release}</div>
+												</div>
+											) : (
+												<div className={styles.description}>
+													<h3>{title(item.title)}</h3>
+													<div>
+														<div className={styles.year}>
+															<p>{item.year}</p>
+														</div>
 
-													<div className={styles.genre}>
-														{announced ? null : item.material_data
-																.anime_genres[1] ? (
-															<>
-																<p>{item.material_data.anime_genres[0]}/</p>{" "}
-																<p>{item.material_data.anime_genres[1]}</p>
-															</>
-														) : (
-															<p>{item.material_data.anime_genres[0]}</p>
-														)}
+														<div className={styles.genre}>
+															{announced ? null : item.material_data
+																	.anime_genres[1] ? (
+																<>
+																	<p>{item.material_data.anime_genres[0]}/</p>{" "}
+																	<p>{item.material_data.anime_genres[1]}</p>
+																</>
+															) : (
+																<p>{item.material_data.anime_genres[0]}</p>
+															)}
+														</div>
 													</div>
 												</div>
-											</div>
-										)}
-									</div>
-								</SwiperSlide>
-							))
-						) : (
-							<SkeletonLoader width={190} height={250} count={1} />
-						)}
-					</Swiper>
-				}
+											)}
+										</div>
+									</SwiperSlide>
+								))
+							) : (
+								<SkeletonLoader width={190} height={250} count={1} />
+							)}
+						</Swiper>
+					}
 
-				<button
-					onClick={nextSlide}
-					className={styles.button}
-					disabled={swiperNavigationAllowed.nextButtonDisabled}
-				>
-					<MaterialIcon name={"MdChevronRight"} />
-				</button>
+					<button
+						onClick={nextSlide}
+						className={styles.button}
+						disabled={swiperNavigationAllowed.nextButtonDisabled}
+					>
+						<MaterialIcon name={"MdChevronRight"} />
+					</button>
+				</div>
 			</div>
 		</section>
 	)
