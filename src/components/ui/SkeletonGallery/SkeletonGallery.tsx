e28d8bba3a0e1fa5c1aadaefbb7b93683f8a3068
NikitaSdev <nikitashadowrazev@gmail.com>
2023-05-31 04:35:46 +0300

    everything done

--- src/components/ui/SkeletonGallery/SkeletonGallery.tsx
+++ src/components/ui/SkeletonGallery/SkeletonGallery.tsx
@@ -13,76 +13,165 @@ const SkeletonGallery = () => {
 						width={200}
 						height={40}
 						baseColor={"#E3E3E3"}
+						highlightColor={"#F1F1F1"}
 						className={styles.rounded}
 					/>
 				</div>
 				<div>
-					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
-					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={17}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
+					<Skeleton
+						count={1}
+						width={200}
+						height={17}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
 				</div>
 			</div>
 			<div className={styles.skeleton}>
 				<div className={styles.bigSkeleton}>
-					<Skeleton count={1} width={200} height={270} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={270}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
 					<Skeleton
 						count={1}
 						width={200}
 						height={40}
 						baseColor={"#E3E3E3"}
+						highlightColor={"#F1F1F1"}
 						className={styles.rounded}
 					/>
 				</div>
 				<div>
-					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
-					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={17}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
+					<Skeleton
+						count={1}
+						width={200}
+						height={17}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
 				</div>
 			</div>
 			<div className={styles.skeleton}>
 				<div className={styles.bigSkeleton}>
-					<Skeleton count={1} width={200} height={270} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={270}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
 					<Skeleton
 						count={1}
 						width={200}
 						height={40}
 						baseColor={"#E3E3E3"}
+						highlightColor={"#F1F1F1"}
 						className={styles.rounded}
 					/>
 				</div>
 				<div>
-					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
-					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={17}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
+					<Skeleton
+						count={1}
+						width={200}
+						height={17}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
 				</div>
 			</div>
 			<div className={styles.skeleton}>
 				<div className={styles.bigSkeleton}>
-					<Skeleton count={1} width={200} height={270} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={270}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
 					<Skeleton
 						count={1}
 						width={200}
 						height={40}
 						baseColor={"#E3E3E3"}
+						highlightColor={"#F1F1F1"}
 						className={styles.rounded}
 					/>
 				</div>
 				<div>
-					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
-					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={17}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
+					<Skeleton
+						count={1}
+						width={200}
+						height={17}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
 				</div>
 			</div>
 			<div className={styles.skeleton}>
 				<div className={styles.bigSkeleton}>
-					<Skeleton count={1} width={200} height={270} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={270}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
 					<Skeleton
 						count={1}
 						width={200}
 						height={40}
 						baseColor={"#E3E3E3"}
+						highlightColor={"#F1F1F1"}
 						className={styles.rounded}
 					/>
 				</div>
 				<div>
-					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
-					<Skeleton count={1} width={200} height={17} baseColor={"#F1F1F1"} />
+					<Skeleton
+						count={1}
+						width={200}
+						height={17}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
+					<Skeleton
+						count={1}
+						width={200}
+						height={17}
+						baseColor={"#F1F1F1"}
+						highlightColor={"#E3E3E3"}
+					/>
 				</div>
 			</div>
 		</div>
--- src/components/ui/mainBanner/SkeletonBanner.tsx
+++ src/components/ui/mainBanner/SkeletonBanner.tsx
@@ -1,8 +1,75 @@
-import React from "react"
 import Skeleton from "react-loading-skeleton"
 
+import styles from "./MainBanner.module.scss"
+
 const SkeletonBanner = () => {
-	return <Skeleton width={"100%"} height={593} baseColor={"#333238"} />
+	return (
+		<div className={styles.skeleton}>
+			<Skeleton
+				width={"100%"}
+				height={593}
+				baseColor={"#333238"}
+				highlightColor={"#1F1F25"}
+				className={styles.background}
+			/>
+			<div className={styles.container}>
+				<div>
+					<Skeleton
+						width={355}
+						height={86}
+						baseColor={"#1F1F25"}
+						highlightColor={"#333238"}
+					/>
+					<Skeleton
+						width={192}
+						height={28}
+						baseColor={"#1F1F25"}
+						highlightColor={"#333238"}
+					/>
+				</div>
+				<div>
+					<Skeleton
+						width={355}
+						height={86}
+						baseColor={"#1F1F25"}
+						highlightColor={"#333238"}
+					/>
+					<Skeleton
+						width={192}
+						height={28}
+						baseColor={"#1F1F25"}
+						highlightColor={"#333238"}
+					/>
+				</div>
+			</div>
+			<div className={styles.skeletonSlider}>
+				<Skeleton
+					width={191}
+					height={285}
+					baseColor={"#1F1F25"}
+					highlightColor={"#333238"}
+				/>
+				<Skeleton
+					width={191}
+					height={285}
+					baseColor={"#1F1F25"}
+					highlightColor={"#333238"}
+				/>
+				<Skeleton
+					width={191}
+					height={285}
+					baseColor={"#1F1F25"}
+					highlightColor={"#333238"}
+				/>
+				<Skeleton
+					width={191}
+					height={285}
+					baseColor={"#1F1F25"}
+					highlightColor={"#333238"}
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default SkeletonBanner
