e4bd5b318f7269ac3fa08e4d7a53ffa85d97f1e3
NikitaSdev <nikitashadowrazev@gmail.com>
2023-06-01 13:16:21 +0300

    final pravki final

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -27,7 +27,9 @@ const Home: FC<IHome> = ({
 				<MainBanner list={list} />
 
 				{isLoading ? (
-					<SkeletonGallery />
+					<div className={"mt-5"}>
+						<SkeletonGallery />
+					</div>
 				) : (
 					<Gallery
 						items={trendingMovies.trendingList}
